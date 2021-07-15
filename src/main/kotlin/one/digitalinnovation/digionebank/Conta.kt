c2647f03c130c19e1a0f15585fabe637b85f12fe
artie021 <arthurfellype21@gmail.com>
2021-07-15 14:44:58 -0300

    Criada a class conta e suas funções

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Conta.kt
@@ -0,0 +1,18 @@
+package one.digitalinnovation.digionebank
+
+
+
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: Double
+) {
+    fun deposito(valor:Double){
+
+    }
+
+    fun saque(valor:Double){
+
+    }
+
+}
